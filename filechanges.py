ba3d1a9fbbe37769fdffae3fd76b43129c4ea20d
Cristian Antonioli <tecnico01ca@gpinformatica.com>
2020-12-24 10:37:06 +0100

    fase 2

--- filechanges.py
+++ filechanges.py
@@ -9,17 +9,110 @@ def getdbname():
 """Si connette al db SQLite"""
 def connectdb():
     try:
-        dbfile = getdbname() 
+        dbfile = getdbname()
         conn = sqlite3.connect(dbfile, timeout=2)
         return conn
     except:
         print("Qualcosa è andato storto")
 
+"""Funzione che esegue query senza connessione alla base"""
+def runcmdnoconn(query, args=None):
+    result = False
+    try:
+        conn = connectdb()
+        if not conn is None:
+            result = runcmd(conn,query,args)
+        conn.close()
+    except:
+        print("errore in runcmdnoconn")
+    return result
+
+"""Funzione base che esegue query senza risultato"""
+def runcmd(conn, query, args=None):
+    args = args or []
+    result = False
+    try:
+        cursor = conn.execute(query, args)
+        conn.commit()
+        cursor.close()
+        result = True
+    except Exception as err:
+        print("errore in esecuzione query")
+        print(err)
+    return result
+
+"""Esiste la tabella?"""
+def tableexists(table):
+    result = False
+    try:
+        conn = connectdb()
+        if not conn is None:
+            query = "SELECT name FROM sqlite_master WHERE type = 'table' AND name = ?"
+            args = (table,)
+            cur = conn.execute(query, args)
+            item = cur.fetchone()
+            result = not item is None
+            cur.close()
+            conn.close()
+    except:
+        print ("tableexist ERROR")
+    return result
+
+"""Crea tabella"""
+def createhashtable():
+    if tableexists('files'):
+        return True
+    query = "CREATE TABLE files (id INTEGER PRIMARY KEY, fname VARCHAR(255) NOT NULL, md5 BLOB NOT NULL)"
+    return runcmdnoconn(query)
+
+"""Crea indice"""
+def createhashtableidx():
+    if tableexists('files'):
+        query = 'CREATE INDEX IF NOT EXISTS idxfile ON files (fname)'
+        return runcmdnoconn(query)
+    return False
+
+"""Update the SQLite File Table"""
+def updatehashtable(fname, md5):
+    qry = "UPDATE files SET md5 = ? WHERE fname = ?"
+    return runcmdnoconn(qry,(md5,fname))
+
+"""Insert into the SQLite File Table"""
+def inserthashtable(fname, md5):
+    qry = "INSERT INTO files (fname, md5) VALUES (?,?)" 
+    return runcmdnoconn(qry, (fname, md5))
+
+"""Setup's the Hash Table"""
+def setuphashtable():
+   createhashtable()
+   createhashtableidx()
+
+"""Checks if md5 hash tag exists in the SQLite DB"""
+def md5indb(fname):
+    items = []
+    if tableexists('files'):
+        try:
+            conn = connectdb()
+            if not conn is None:
+                qry = "SELECT md5 FROM files WHERE fname = ?"
+                try:
+                    cursor = conn.execute(qry,(fname,))
+                    for item in cursor:
+                        items.append(item[0])
+                except Exception as ex:
+                    print(ex)
+        except:
+            pass
+    return items
 
 """Test"""
 if __name__ == "__main__":
     try:
-        cnn = connectdb()
-        cnn.close()
+        setuphashtable()
+        print(inserthashtable('test', bytearray([1,2,3,4,5])))
+        print(md5indb("test"))
+        print(updatehashtable('test', bytearray([1,2,3,4,6])))
+        print(md5indb("test"))
+
     except:
         print("Nulla da dichiarare")
